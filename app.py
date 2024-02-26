b8faa9f74c18735fbac109626808934e9eacaf3f
Samarth Varu <samarthvaru111@gmail.com>
2024-02-26 17:48:40 -0500

    updated port and added dockerfile

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def schedule():
     return jsonify({'processes': result})  # Modified to return processed data
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
